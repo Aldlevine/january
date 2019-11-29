058186da36d25350fd77086dfc97f7bd574586d4
Aaron Levine <ald.levine@gmail.com>
2019-11-29 10:32:44 -0500

    fix layer_shifter not shifting layers correctly

--- engine/layer_shifter.gd
+++ engine/layer_shifter.gd
@@ -7,21 +7,17 @@ const env_bits = 1047552 #0b11111111110000000000
 func shift_layer(obj, vpos):
   var tier = int(floor(vpos / LAYER_HEIGHT))
   var parts = obj.base_layer & base_bits
-  var mask = obj.base_layer & ~parts
   parts = parts << tier
-  obj.collision_layer = mask | (parts & base_bits)
+  obj.collision_layer = parts & base_bits
 
   parts = obj.base_layer & env_bits
-  mask = obj.base_layer & ~parts
   parts = parts << tier
-  obj.collision_layer = mask | (parts & env_bits)
+  obj.collision_layer |= parts & env_bits
 
   parts = obj.base_mask & base_bits
-  mask = obj.base_mask & ~parts
   parts = parts << tier
-  obj.collision_mask = mask | (parts & base_bits)
+  obj.collision_mask = parts & base_bits
 
   parts = obj.base_mask & env_bits
-  mask = obj.base_mask & ~parts
   parts = parts << tier
-  obj.collision_mask = mask | (parts & env_bits)
+  obj.collision_mask |= parts & env_bits
